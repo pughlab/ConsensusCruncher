01035f0b73518ad7c3224d2a7903b3ab118a29e2
Tingting Wang <ttwang@mordor.(none)>
2017-08-10 19:10:06 -0400

    Code clean up removing unused lines and added explanations.

--- DCS_maker.py
+++ DCS_maker.py
@@ -2,39 +2,49 @@
 
 ###############################################################
 #
-#                          DCS Maker
+#           Duplex Consensus Sequence (DCS) Generator
 #
 # Author: Nina Wang
-# Last Modified: May 18, 2016
 # Date Created: Mar 24, 2016
 ###############################################################
 # Function:
+# To generate duplex/double-strand consensus sequences for molecule based error suppression.
+# - Consensus sequence from bases with Phred quality >= 30
+# - Consensus quality score from addition of quality scores (i.e. product of error probabilities)
+#
 # Written for Python 3.5.1
 #
-# Inputs:
-# 1. A position-sorted paired-end BAM file containing reads with a duplex tag
-#    in the header.
+# Usage:
+# Python3 DCS_maker.py [--infile INFILE] [--outfile OUTFILE]
 #
-# Outputs:
-# 1. A paired-end BAM file containing single stranded consensus sequences.
-# 2. A text file containing summary statistics (DCS reads)
-#    - Read family: reads that share the same molecular barcode, chr, and start
-#                   coordinates for Read1 and Read2
-#    - Singleton: a read family containing only one member (a single read)
-#
-# usage: DCS_maker.py [--infile INFILE]
-#                      [--outfile OUTFILE]
-# optional arguments:
+# Arguments:
 # --infile INFILE     input BAM file
 # --outfile OUTFILE   output BAM file
+# --bedfile BEDFILE   Bedfile containing coordinates to subdivide the BAM file (Recommendation: cytoband.txt -
+#                     See bed_separator.R for making your own bed file based on a target panel / specific coordinates)
 #
-# To run script: python3 $cwd/DCS_maker.py --infile $identifier.sscs.bam --outfile $identifier.dcs.bam
+# Inputs:
+# 1. A position-sorted BAM file containing paired-end reads with SSCS consensus identifier in the header/query name
+# 2. A BED file containing coordinates subdividing the entire ref genome for more manageable data processing
 #
-# python3 DCS_maker.py --infile MEM-001_KRAS.sscs.bam --outfile MEM-001_KRAS.dcs.bam
+# Outputs:
+# 1. A BAM file containing paired double stranded consensus sequences - "dcs.bam"
+# 2. A SSCS singleton BAM file containing SSCSs without reads from the complimentary strand - "sscs.singleton.bam"
+# 3. A bad read BAM file containing unpaired, unmapped, and multiple mapping reads - "dcs.badReads.bam"
+# 4. A text file containing summary statistics (Total SSCS reads, Unmmaped SSCS reads, Secondary/Supplementary SSCS
+#    reads, DCS reads, and SSCS singletons) - "stats.txt" (Stats pended to same stats file as SSCS)
+#
+# Concepts:
+#    - Read family: reads that share the same molecular barcode, chr, and start
+#                   coordinates for Read1 and Read2
+#    - SSCS Singleton: a SSCS read without its complimentary read
 #
 ###############################################################
 
-import pysam # Need to install
+##############################
+#        Load Modules        #
+##############################
+import pysam  # Need to install
 import collections
 import re
 import array
@@ -44,13 +54,12 @@ import math
 
 from consensus_helper import *
 
+
 ###############################
 #       Helper Functions      #
 ###############################
-
-
 def dcs_consensus_tag(tag, ds):
-    '''(str, str) -> str
+    """(str, str) -> str
     Return consensus tag for duplex reads.
 
     >>> dcs_consensus_tag('TTCA_7_55259315_7_55259454_98M_98M_neg:3', 'CATT_7_55259315_7_55259454_98M_98M_pos:6')
@@ -61,7 +70,7 @@ def dcs_consensus_tag(tag, ds):
 
     >>> dcs_consensus_tag('TTTC_7_140477735_7_140477790_98M_98M_neg:3', 'TCTT_7_140477735_7_140477790_98M_98M_pos:2')
     'TCTT_TTTC_7_140477735_7_140477790_98M_98M'
-    '''
+    """
     barcode = tag.split('_')[0]
     duplex_barcode = ds.split('_')[0]
     tag_coor = tag.split('_', 1)[1].rsplit('_', 1)[0]
@@ -74,27 +83,28 @@ def dcs_consensus_tag(tag, ds):
 
 
 def duplex_consensus(read1, read2):
-    '''(pysam.calignedsegment.AlignedSegment, pysam.calignedsegment.AlignedSegment) -> pysam.calignedsegment.AlignedSegment
+    """(pysam.calignedsegment.AlignedSegment, pysam.calignedsegment.AlignedSegment) -> pysam.calignedsegment.AlignedSegment
 
-    Return consensus of 2 reads with N for variant bases.
-    '''
+    Return consensus of complimentary reads with N for inconsistent bases.
+    """
     consensus_seq = ''
-    qual_consensus = []
+    consensus_qual = []
 
     for i in range(read1.query_length):
+        # Check to see if base at position i is the same
         if read1.query_sequence[i] == read2.query_sequence[i]:
             consensus_seq += read1.query_sequence[i]
-            P = 10**(-(read1.query_qualities[i]/10)) * 10**(-(read2.query_qualities[i]/10))
-            Q = round(-10 * math.log10(P))
-            if Q > 62:
-                qual_consensus += [62]
+            mol_qual = sum(read1.query_qualities[i], read2.query_qualities[i])
+            # Set to max quality score if sum of qualities is greater than the threshold (Q60) imposed by genomic tools
+            if mol_qual > 60:
+                consensus_qual += [60]
             else:
-                qual_consensus += [Q]
+                consensus_qual += [mol_qual]
         else:
             consensus_seq += 'N'
-            qual_consensus += [0]
+            consensus_qual += [0]
 
-    return consensus_seq, qual_consensus
+    return consensus_seq, consensus_qual
 
 
 ###############################
@@ -104,32 +114,42 @@ def duplex_consensus(read1, read2):
 def main():
     # Command-line parameters
     parser = ArgumentParser()
-    parser.add_argument("--infile", action = "store", dest="infile", help="input BAM file", required = True, type = str)
-    parser.add_argument("--outfile", action = "store", dest="outfile", help="output BAM file", required = True, type = str)
-    parser.add_argument("--bedfile", action = "store", dest="bedfile", help="output BAM file", required = False, type = str)
+    parser.add_argument("--infile", action = "store", dest="infile", help="Input BAM file", required=True)
+    parser.add_argument("--outfile", action = "store", dest="outfile", help="Output BAM file", required=True)
+    parser.add_argument("--bedfile", action="store", dest="bedfile",
+                        help="Bedfile containing coordinates to subdivide the BAM file (Recommendation: cytoband.txt - \
+                        See bed_separator.R for making your own bed file based on a target panel/specific coordinates)",
+                        required=False)
     args = parser.parse_args()
 
     start_time = time.time()
 
+    # ===== Initialize input and output bam files =====
     args.infile = str(args.infile)
     args.outfile = str(args.outfile)
 
     SSCS_bam = pysam.AlignmentFile(args.infile, "rb")
-    DCS_bam = pysam.AlignmentFile(args.outfile, "wb", template = SSCS_bam)
+    DCS_bam = pysam.AlignmentFile(args.outfile, "wb", template=SSCS_bam)
     
     if re.search('dcs.sr', args.outfile):
-		SSCS_singleton = pysam.AlignmentFile('{}.sscs.sr.singleton.bam'.format(args.outfile.split('.dcs.sr')[0]), "wb", template = SSCS_bam)
-		badRead_bam = pysam.AlignmentFile('{}.dcs.sr.badReads.bam'.format(args.outfile.split('.dcs.sr')[0]), "wb", template = SSCS_bam)    	
-		dcs.header = "DCS - Singleton Rescue"
+        SSCS_singleton = pysam.AlignmentFile('{}.sscs.sr.singleton.bam'.format(args.outfile.split('.dcs.sr')[0]),
+                                             "wb", template=SSCS_bam)
+        badRead_bam = pysam.AlignmentFile('{}.dcs.sr.badReads.bam'.format(args.outfile.split('.dcs.sr')[0]),
+                                          "wb", template=SSCS_bam)
+        dcs_header = "DCS - Singleton Rescue"
+        sr_header = " SR"
     else:
-		SSCS_singleton = pysam.AlignmentFile('{}.sscs.singleton.bam'.format(args.outfile.split('.dcs')[0]), "wb", template = SSCS_bam)
-		badRead_bam = pysam.AlignmentFile('{}.dcs.badReads.bam'.format(args.outfile.split('.dcs')[0]), "wb", template = SSCS_bam)
-		dcs.header = "DCS"
+        SSCS_singleton = pysam.AlignmentFile('{}.sscs.singleton.bam'.format(args.outfile.split('.dcs')[0]),
+                                             "wb", template=SSCS_bam)
+        badRead_bam = pysam.AlignmentFile('{}.dcs.badReads.bam'.format(args.outfile.split('.dcs')[0]),
+                                          "wb", template=SSCS_bam)
+        dcs_header = "DCS"
+        sr_header = ""
 
     stats = open('{}.stats.txt'.format(args.outfile.split('.dcs')[0]), 'a')
     time_tracker = open('{}.time_tracker.txt'.format(args.outfile.split('.dcs')[0]), 'a')
 
-    # ===== Initialize dictionaries and convert SSCS data into dict=====
+    # ===== Initialize dictionaries and counters=====
     read_dict = collections.OrderedDict()
     tag_dict = collections.defaultdict(int)
     pair_dict = collections.defaultdict(list)
@@ -137,9 +157,9 @@ def main():
 
     unmapped = 0
     unmapped_mate = 0
-    multiple_mapping = 0  # secondary/supplementary reads
+    multiple_mapping = 0  # Secondary/supplementary reads
     counter = 0
-    SSCS_singletons = 0
+    SSCS_singletons = 0  # Single strand consensus sequences without a complimentary strand
     multiple_mappings = 0
 
     duplex_count = 0
@@ -188,6 +208,7 @@ def main():
         # ===== Create consenus seq for reads =====
         for readPair in list(csn_pair_dict.keys()):
             if len(csn_pair_dict[readPair]) != 2:
+                # This shouldn't be a problem as our SSCS should contain read pairs
                 print('uh oh pairing problem!!! Only one read, mate missing')
                 print(readPair)
                 print(csn_pair_dict[readPair][0])
@@ -195,7 +216,7 @@ def main():
                 print(SSCS_bam.mate(read_dict[csn_pair_dict[readPair][0]][0]))
             else:
                 for tag in csn_pair_dict[readPair]:
-                    # === Determine tag of duplex pair read ===
+                    # Determine tag of duplex read
                     ds = duplex_tag(tag)
 
                     # === Group duplex read pairs and create consensus ===
@@ -205,16 +226,16 @@ def main():
                             duplex_count += 1
 
                             # consensus seq
-                            consensus_seq, qual_consensus = duplex_consensus(read_dict[tag][0], read_dict[ds][0])
+                            consensus_seq, consensus_qual = duplex_consensus(read_dict[tag][0], read_dict[ds][0])
 
                             # consensus duplex tag
-                            dcs_query_name = dcs_consensus_tag(tag, ds)
+                            dcs_query_name = dcs_consensus_tag(tag, ds)  # New query name containing both barcodes
 
                             dcs_read = create_aligned_segment([read_dict[tag][0], read_dict[ds][0]], consensus_seq,
-                                                              qual_consensus, dcs_query_name)
+                                                              consensus_qual, dcs_query_name)
 
+                            # add duplex tag to dictionary to prevent making a duplex for the same sequences twice
                             duplex_dict[tag] += 1
-                            # duplex_dict[tag] = dcs_read  # add duplex tag to dictionary to prevent making a duplex for the same sequences twice
 
                             DCS_bam.write(dcs_read)
 
@@ -228,20 +249,21 @@ def main():
                 # Remove key from dictionary after writing
                 del csn_pair_dict[readPair]
 
-    print(collections.Counter([i for i in tag_dict.values()]))  ## WHY ARE SOME VALUES 0 IN THE COUNTER?
+    summary_stats = '''# === {} ===
+SSCS{} - Total reads: {}
+SSCS{} - Unmapped reads: {}
+SSCS{} - Secondary/Supplementary reads: {}
+DCS{} reads: {}
+SSCS{} singletons: {} \n'''.format(dcs_header, sr_header, counter, sr_header, unmapped, sr_header, multiple_mappings,
+                                   sr_header, duplex_count, sr_header, SSCS_singletons)
+    stats.write(summary_stats)
+    print(summary_stats)
+
+    # Output total DCS time
     time_tracker.write('DCS: ')
     time_tracker.write(str((time.time() - start_time)/60) + '\n')
 
-    summary_stats = '''# === {} ===
-SSCS - Total reads: {}
-SSCS - Unmapped reads: {} 
-SSCS - Secondary/Supplementary reads: {}
-DCS reads: {}
-SSCS singletons: {} \n'''.format(dcs.header, counter, unmapped, multiple_mappings, duplex_count, SSCS_singletons)
-    stats.write(summary_stats)
-
-    print(summary_stats)
-
+    # Close files
     time_tracker.close()
     stats.close()
     DCS_bam.close()
