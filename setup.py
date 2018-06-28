9f8cd35175b44312e6b8cd06b48b9c91112f3b45
Tingting Wang <ttwang@mordor.(none)>
2018-06-28 15:25:13 -0400

    Created python setup file and easy installation of required dependencies

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(name='ConsensusCruncher',
+      version='0.0.1',
+      author="Nina Wang",
+      author_email="nina.tt.wang@gmail.com",
+      description="Barcoding-based error suppression algorithm",
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url="https://github.com/pughlab/ConsensusCruncher",
+      packages=["consensuscruncher"],
+      install_requires=['numpy', 'pandas', 'pysam', 'Biopython'],
+      classifiers="Programming Language :: Python :: 3"
+      )
