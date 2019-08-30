d1f568b466bcadc0b4c42be1f4b27ebb578da416
Nina <nina@tpugh21.uhnres.utoronto.ca>
2019-08-30 15:27:38 -0400

    Update readthedocs versioning

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,7 +26,7 @@ author = 'Nina Wang'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '0.0.1'
+release = '0.0.2'
 
 
 # -- General configuration ---------------------------------------------------
