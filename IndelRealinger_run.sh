ed303a87b81e93ee27e04c5dae46cebe5df67a6c
arnavaz <arnavazdanesh@gmail.com>
2021-11-19 14:37:37 -0500

    Update IndelRealinger_run.sh

--- IndelRealinger_run.sh
+++ IndelRealinger_run.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
+bam=#bamfile name ( you can loop through a list of bam file by doing for bam in $(cat bamfiles);do 
 module load gatk/3.3-0 
 module load igenome-human/hg38
-input=/cluster/projects/pughlab/projects/M4/ConsensusCruncher/processed/Genes_bams
-bed=/cluster/projects/pughlab/projects/M4/Bed_files/pughlab_panels/MYLSTONE2_Gene_GRCh38.bed
-cwd=/cluster/projects/pughlab/projects/M4/ConsensusCruncher/processed
+input=## bam directory from the fastqTobam steph
+bed=## bed file
+cwd=## currecnt working directory where your processed bam files will go to
 sites=/cluster/projects/pughlab/references/broad_homo_sapien_known_sites/Homo_sapiens_assembly38.known_indels.vcf.gz
 
 mkdir -p $cwd/intervals
-
+mkdir -p $cwd/output
 gatk -T  RealignerTargetCreator \
     -R $REF  \
     -L $bed \
@@ -18,5 +19,5 @@ gatk -T  RealignerTargetCreator \
 java -jar $gatk_dir/GenomeAnalysisTK.jar -T IndelRealigner \
     -R $REF \
     -targetIntervals $cwd/intervals/${1}.bed  \
-    -known $sites -I /cluster/projects/pughlab/projects/M4/ConsensusCruncher/processed/all_bams/$1 \
+    -known $sites -I /cluster/projects/pughlab/projects/M4/ConsensusCruncher/processed/all_bams/$bam \
     -o $output/$1
