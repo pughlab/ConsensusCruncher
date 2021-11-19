e69b6d6c11100c4af4abb412afca61a5d170ae71
arnavaz <arnavazdanesh@gmail.com>
2021-11-19 14:51:20 -0500

    Update script_gen_consensus.sh

--- script_gen_consensus.sh
+++ script_gen_consensus.sh
@@ -1,17 +1,16 @@
 INPUT=#IndelRealigned bams
 OUTPUT=# Output directory
-CODEDIR="/mnt/work1/users/pughlab/bin/ConsensusCruncher/" # ConsensusCruncher code directory
-CONFIG==config.ini"  # Path to config file
+CODEDIR=# ConsensusCruncher code directory
+CONFIG=config.ini  # Path to config file
 CYTOBAND=$CODEDIR/ConsensusCruncher/hg38_cytoBand.txt # Textfile to separate
 
 QSUBDIR=$OUTPUT/consensus/qsub
 
 mkdir $OUTPUT/consensus
 mkdir $QSUBDIR
-for filename in $(ls $INPUT | grep -v bai); do
+for filename in $(ls $INPUT | grep -v bai);
+do
 echo -e "#!/bin/bash\n \nmodule load python3/3.7.5\n" > $QSUBDIR/$filename.sh
-###############
-    #  consensus  #
-    ###############
-    echo -e "python3 $CODEDIR/ConsensusCruncher.py -c $CONFIG consensus -i $INPUT/$filename -o $OUTPUT/consensus -b $CYTOBAND" >> $QSUBDIR/$filename.sh
-    done
+
+echo -e "python3 $CODEDIR/ConsensusCruncher.py -c $CONFIG consensus -i $INPUT/$filename -o $OUTPUT/consensus -b $CYTOBAND" >> $QSUBDIR/$filename.sh
+done
